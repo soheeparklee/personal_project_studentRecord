717ad3fbbf5d91654a78c6ac8f0408cbb55c3d9d
soheeparklee <soheeparklee@gmail.com>
2023-10-19 16:15:47 +0900

    feat: add timestamp
    current time을 가져와서 거기서 insertAt서버에서 받아온 시간을 뺴면 = 몇 분 전에 업로드 했는지 알 수 있음
    - universal time이기 떄문에 -9
    - recent update to come up => array.reverse

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,57 @@
+const calcTime= (timestamp) =>{
+    const curTime= new Date().getTime() - 9*60*60*1000;
+    const time= new Date(curTime-timestamp);
+    const hour= time.getHours();
+    const minutes= time.getMinutes();
+    const seconds= time.getSeconds();
+
+    if (hour>0) return `${hour}시간`
+    else if (minutes>0) return `${minutes}분`
+    else if(seconds >= 0 ) return `${seconds}초` 
+    else return "방금";  
+
+};
+
 const renderData= (data) => {
     const main= document.querySelector("main");
-    data.forEach((obj) => {
-        const div= document.createElement("div");
-        div.className= "main-box"
-        div.innerText= obj.name;
-        main.appendChild(div);
+    data.reverse().forEach((obj) => {
+        const mainBoxDiv= document.createElement("div");
+        mainBoxDiv.className= "main-box"
+        main.appendChild(mainBoxDiv);
+
+        const studentMetaDiv= document.createElement("div");
+        studentMetaDiv.className= "main-box-student-meta"
+        mainBoxDiv.appendChild(studentMetaDiv);
+
+        const metaName= document.createElement("div");
+        metaName.className= "student-name"
+        metaName.innerText= obj.name;
+        studentMetaDiv.appendChild(metaName);
+        const metaGrade= document.createElement("div");
+        metaGrade.className= "student-grade"
+        metaGrade.innerText= `${obj.grade}학년`
+        studentMetaDiv.appendChild(metaGrade);
+        const metaClass= document.createElement("div");
+        metaClass.className= "student-class"
+        metaClass.innerText= obj.class;
+        studentMetaDiv.appendChild(metaClass);
+        const metaGender= document.createElement("div");
+        metaGender.className= "student-gender"
+        metaGender.innerText= obj.gender;
+        studentMetaDiv.appendChild(metaGender);
+
+        const metaTime= document.createElement("div");
+        metaTime.className= "student-gender"
+        metaTime.innerText= `${calcTime(obj.insertAt)} 전에 등교하였습니다.`;
+        studentMetaDiv.appendChild(metaTime);
+
+        const studentImgDiv= document.createElement("div");
+        studentImgDiv.className= "main-box-student-img"
+        mainBoxDiv.appendChild(studentImgDiv);
+        const studentImg= document.createElement("img");
+        studentImg.src= "assets/student.svg"
+        studentImgDiv.appendChild(studentImg);
+        
         
     });
 }
